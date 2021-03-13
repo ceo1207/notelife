2e300031c03eac4877b097a2cf5f55abdc4c59e0
ceo1207 <water799@126.com>
2021-03-13 21:46:52 +0800

    python

--- python_1.py
+++ python_1.py
@@ -6,3 +6,28 @@ x_mat_reverse = np.mat([[1, 0, 0, 0], [0, cos(-x_angle), sin(x_angle), 0], [0, -
 z_mat = np.mat([[cos(2), sin(2), 0, 0], [-sin(2), cos(2), 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
 # x_mat * z_mat != z_mat * x_mat
 # x_mat * x_mat_reverse = I
+
+def count():
+    funcs = []
+    for i in [1, 2, 3]:
+        def g():
+            tmp = int(i)    # 这里创建了一个匿名函数
+            return tmp
+        funcs.append(g)        # 将循环变量的值传给 g
+    return funcs
+
+def count():
+    funcs = []
+    for i in [1, 2, 3]:
+        def g(param):
+            tmp = lambda : param    # 这里创建了一个匿名函数
+            return tmp
+        # 必须要让循环中的i即时生效出来，而不是在函数延迟调用时才访问i的值
+        funcs.append(g(i))        # 将循环变量的值传给 g
+    return funcs
+
+
+f1, f2, f3 = count()
+f1()
+f2()
+f3()
